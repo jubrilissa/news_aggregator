9f04c12442f212a952f746512395d336e9ef9f81
Jubril Issa <issa.jubril@andela.com>
2020-04-16 09:10:05 +0100

    Add news source to the news model

--- news/models.py
+++ news/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-
-# Create your models here.
-
 class News(models.Model):
     headline = models.CharField(max_length=100)
     content = models.TextField()
     category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
     published = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    source = models.ForeignKey('outlet.Outlet', on_delete=models.CASCADE)
+    link = models.URLField()
