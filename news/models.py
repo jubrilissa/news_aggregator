90072fde6a29053e9ffb858ff4eee34351f16e87
Jubril Issa <issa.jubril@andela.com>
2020-04-16 02:12:21 +0100

    Add the news model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class News(models.Model):
+    headline = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
